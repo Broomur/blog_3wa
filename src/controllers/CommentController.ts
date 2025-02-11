4ecb0fc605ab86aa85be425b604e0077184fbdbf
MarianneKaiu <lapierre.marianne@gmail.com>
2025-02-11 14:19:54 +0100

    create Comment

--- src/controllers/CommentController.ts
+++ src/controllers/CommentController.ts
@@ -1,4 +1,4 @@
-
+import { Request, Response } from 'express';
 import Comment from "../models/Comment";
 import { Op } from "sequelize";
 
@@ -18,15 +18,35 @@ class CommentController {
   // static async userComments(req: Request, res: Response): Promise<void> {
 
   // }
-  // static async createComment(req: Request, res: Response): Promise<void> {
-
-  // }
+  static async createComment(req: Request, res: Response): Promise<void> {
+    const articleId = Number(req.path.split('=')[1]);  
+    const content = req.body
+    try {
+      const comment = await Comment.create(
+        {
+          content
+        }
+      )
+      res.redirect(`/article/detail/${articleId}`)
+    } catch (error) {
+			res.render('misc/erreur', {title: '500', message: 'erreur serveur :('})
+  }
   // static async updateComment(req: Request, res: Response): Promise<void> {
 
   // }
-  // static async deleteComment(req: Request, res: Response): Promise<void> {
-
+  // static async deleteComment(commentId: number) {
+  //   try {
+  //     const comment = await Comment.findAll({
+  //       where: {
+  //         article_id: { [Op.eq]: commentId}
+  //       }
+  //     }
+  //   )}
+  //   catch(error) {
+  //     console.error("Erreur lors de la récupération du commentaire :", error)
+  //   }
+  // } 
 }
-
+}
 
 export default CommentController
